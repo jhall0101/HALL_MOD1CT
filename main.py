1a0569b36e772c25a05d8a587d262d9aa50e46b2
AUKTOW <127621231+AUKTOW@users.noreply.github.com>
2023-11-18 09:21:09 -0600

    created python script for simple input and calculation

---
+++ main.py
@@ -0,0 +1,19 @@
+# PART 1
+num1 = float(input("Enter first number (num1): "))
+num2 = float(input("Enter second number (num2): "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print("Addition of num1 and num2 is:", addition)
+print("Subtraction of num1 from num2 is:", subtraction)
+
+# PART 2
+num1 = float(input("Enter first number (num1): "))
+num2 = float(input("Enter second number (num2): "))
+
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Multiplication of num1 and num2 is:", multiplication)
+print("Division of num1 by num2 is:", division)
